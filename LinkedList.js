6282b0fbe8aedc6533bc425bcbede9969563338a
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-15 19:52:55 +0200

    prepared linked list data structure environment ( Node - LinkedList class )

---
+++ LinkedList.js
@@ -0,0 +1,16 @@
+//* LinkedList
+
+//* Node
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+}
