ecf3bc492d1e9acc4e6c94fed53b7d90f69c7f73
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-16 00:52:18 +0200

    Finish getPrevCurrNextByIndex function and it's working successfully

--- LinkedList.js
+++ LinkedList.js
@@ -33,7 +33,7 @@ class LinkedList {
       return this.head;
     }
     let current = this.head;
-    while (current) {
+    while (current.next) {
       current = current.next;
     }
     current.next = node;
@@ -51,4 +51,62 @@ class LinkedList {
     this.head = node;
     return this.head;
   }
+
+  getPrevCurrNextByIndex(index) {
+    if (index === 0) {
+      if (this.head) {
+        return {
+          prev: null,
+          curr: this.head,
+          next: his.head.next,
+        };
+      }
+      return { prev: null, curr: null, next: null };
+    }
+
+    if (index > 0) {
+      if (this.head) {
+        let count = 0;
+        let prev = this.head;
+        let curr;
+        let next;
+
+        while (prev && count < index - 1) {
+          prev = prev.next;
+          count++;
+        }
+        curr = prev.next;
+        next = curr.next;
+        return { prev, curr, next };
+      }
+      return { prev: null, curr: null, next: null };
+    }
+  }
+
+  test(index) {
+    const { prev, curr, next } = this.getPrevCurrNextByIndex(index);
+    return { prev, curr, next };
+  }
+
+  getPrevCurrNextByValue(index) {}
+
+  display() {
+    let str = '';
+    let prev = this.head;
+    while (prev) {
+      str += ' => ' + prev.value;
+      prev = prev.next;
+    }
+    return str;
+  }
 }
+
+const linkedList = new LinkedList();
+
+linkedList.append(1);
+linkedList.prepend(2);
+linkedList.append(3);
+linkedList.prepend(4);
+
+console.log(linkedList.display());
+console.log(linkedList.test(3));
